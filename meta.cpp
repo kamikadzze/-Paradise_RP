5d497746bce1bce67b79d642c94a9bba0d4d32cf
kamikadzze <39806019+kamikadzze@users.noreply.github.com>
2022-07-17 21:54:33 +0300

    Add files via upload

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 1842190943;
+name = "@Paradise RP";
+timestamp = 5248711108087561968;
---
+++ mod.cpp
@@ -0,0 +1,11 @@
+name = "Paradise_RP";
+picture = "paradise_logo.paa";
+logo = "paradise_logo.paa";
+logoOver = "paradise_logo.paa";
+overviewPicture = "paradise_logo.paa";
+overview = "Данный мод необходим для игры на русском RolePlay-сервере Paradise Altis Life. Всю необходимую информацию вы можете получить на нашем сайте. Незабудьте так же посетить наш форум, где можно ознакомиться с правилами сервера и получить ответ на все интересующие вас вопросы.";
+hidePicture = "false";
+hideName = "false";
+actionName = "Website";
+action = "Paradise-rpg.ru";
+description = "Paradise RP";
